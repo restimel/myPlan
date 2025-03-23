1d8b37f427f1f10f8314042bcc1e7aa98dc81762
Benoit Mariat <benoit.mariat@intersec.com>
2025-03-24 00:50:25 +0100

    fill "about"

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>About MyPlan</h1>
+
+        <label>
+            Author:
+            <output>Benoît Mariat</output>
+        </label>
+        <label>
+            Licence:
+            <output>MIT</output>
+        </label>
+
+        <figure>
+            <img src="@/assets/url.png" alt="url to https://restimel.github.io/myPlan">
+            <figcaption>
+                https://restimel.github.io/myPlan
+            </figcaption>
+        </figure>
+    </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+.about {
+  padding: var(--section-padding);
+}
+
+label {
+  display: block;
+}
+
+figure {
+  text-align: center;
 }
 </style>
