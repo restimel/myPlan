1602223cc6e4513942584e24d17dc5d696319a80
Benoit Mariat <benoit.mariat@intersec.com>
2025-03-26 00:24:05 +0100

    fix TS & eslint issues

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -1,4 +1,4 @@
-export function getDistance([x1, y1]: Point, [x2, y2]): number {
+export function getDistance([x1, y1]: Point, [x2, y2]: Point): number {
     const dx = x2 - x1;
     const dy = y2 - y1;
 
