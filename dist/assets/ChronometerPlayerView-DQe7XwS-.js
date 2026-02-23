62d55f1344c561eda4db229fff306f04138eeb26
Benoit Mariat <b.mariat@gmail.com>
2026-02-23 22:19:04 +0100

    update minor production version

---
+++ dist/assets/ChronometerPlayerView-DQe7XwS-.js
@@ -0,0 +1,1 @@
+import{C as e}from"./ChronometerPlayer-D_k0qqdY.js";import{d as o,c as r,b as t,o as a,_ as s}from"./index-B7ob2rh0.js";const c={class:"container view-page"},n=o({__name:"ChronometerPlayerView",setup(_){return(m,p)=>(a(),r("div",c,[t(e)]))}}),l=s(n,[["__scopeId","data-v-66671c39"]]);export{l as default};
