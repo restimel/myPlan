d7d72e73612f9e435aa1f9b9c780545a9acccf51
Benoit Mariat <b.mariat@gmail.com>
2026-04-12 23:32:51 +0200

    update test version

--- test/assets/ChronometerPlayerView-DwTipVHM.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./ChronometerPlayer-B-ERPTuN.js";import{d as o,c as r,b as t,o as a,_ as s}from"./index-CU50uTKl.js";import"./keepScreenAwake-BU2Rqckf.js";const c={class:"container view-page"},n=o({__name:"ChronometerPlayerView",setup(_){return(m,p)=>(a(),r("div",c,[t(e)]))}}),f=s(n,[["__scopeId","data-v-66671c39"]]);export{f as default};
