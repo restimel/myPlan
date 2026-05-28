ed52beb5977aa6a3507a8b6dbf85794e785072e3
Benoit Mariat <b.mariat@gmail.com>
2026-05-28 10:06:20 +0200

    improve upgrade in test version

--- vite.config.test.ts
+++ vite.config.test.ts
@@ -15,6 +15,16 @@ export default defineConfig({
         __CONTEXT__: '"Test"',
     },
     plugins: [
+        {
+            name: 'version-json',
+            generateBundle() {
+                this.emitFile({
+                    type: 'asset',
+                    fileName: 'version.json',
+                    source: JSON.stringify({ version: pkg.version }),
+                });
+            },
+        },
         vue(),
         vueDevTools(),
         VitePWA({
